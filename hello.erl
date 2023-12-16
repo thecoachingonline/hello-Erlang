c31da92518ed8961d2052ff3a9bf60998a2db132
Y.Nanthachai <thecoachingonline@gmail.com>
2023-12-16 02:21:36 +0000

    4096 Bits

--- hello.erl
+++ hello.erl
@@ -1,5 +1,24 @@
--module(helloworld).
--export([hello/0]).
+-module(lattice_cryptography).
 
-hello() ->
-    io:fwrite("Hello World\n").
+-export([encrypt/2, decrypt/2]).
+
+-define(N, 4096).
+
+encrypt(plaintext, key) ->
+    {lwe_params, {n, m, q}} = ntru_encrypt:generate_params(?N),
+    ciphertext = ntru_encrypt:encrypt(lwe_params, plaintext, key),
+    ciphertext.
+
+decrypt(ciphertext, key) ->
+    {lwe_params, {n, m, q}} = ntru_encrypt:generate_params(?N),
+    plaintext = ntru_encrypt:decrypt(lwe_params, ciphertext, key),
+    plaintext.
+
+plaintext = "สวัสดีชาวโลก".
+key = "my_secret_key".
+
+ciphertext = encrypt(plaintext, key).
+
+plaintext2 = decrypt(ciphertext, key).
+
+io:format("plaintext: ~s~n", [plaintext2]).
